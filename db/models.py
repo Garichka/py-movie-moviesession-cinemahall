f3a404b87c55fb9c8caed0e9da01e24476248b3a
Artem <artem90078@gmail.com>
2026-02-13 19:12:45 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
-
     def __str__(self) -> str:
         return self.name
 
@@ -11,7 +10,6 @@ class Genre(models.Model):
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
@@ -19,8 +17,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -32,11 +30,25 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     @property
-    def capacity(self) -> None:
+    def capacity(self) -> int:
         return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {self.show_time:%Y-%m-%d %H:%M:%S}"
